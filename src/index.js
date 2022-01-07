6018bd9a8a8ff6b33115d985fda7678e987765d9
YliaVaininlovich <Ylia.v@tut.by>
2022-01-07 14:38:27 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) n = -1 * n;
+
+    var m = 0;
+    while (n > 0) {
+        m = m * 10;
+        m = m + (n % 10);
+        n = Math.floor(n / 10);
+    }
+
+    return m;
+};
